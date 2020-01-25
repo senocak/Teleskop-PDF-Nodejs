7174fd3213aa4dd1667ff56245d568390defd48d
Anıl Şenocak <asenocak@netas.com.tr>
2020-01-25 17:52:24 +0300

    Instagram done for #1

--- index.js
+++ index.js
@@ -166,6 +166,49 @@ app.get('/twitter', (req, res) => {
         });
     }))
 })
+app.get('/instagram', (req, res) => {
+    const token = req.query.token
+    const stream_id = req.query.stream_id
+    const start_date = req.query.start_date
+    const end_date = req.query.end_date
+    var current = new Date(start_date)
+    const betweenDays = Math.round(Math.abs((new Date(start_date) - new Date(end_date)) / (24 * 60 * 60 * 1000)));
+    var lastWeekStart = (new Date(current.setDate(current.getDate() - betweenDays)).toISOString()).slice(0, -1)
+    var lastWeekEnd = start_date
+    axios.defaults.headers.common['Authorization'] = 'Bearer '+token;
+    axios.all([
+        axios.get('https://apiv2.teleskop.app/v2.0/streams/'+stream_id+'/instagram/stats/histogram?end_date='+end_date+'&start_date='+start_date),
+        axios.get('https://apiv2.teleskop.app/v2.0/streams/'+stream_id+'/instagram/stats/histogram?end_date='+lastWeekEnd+'&start_date='+lastWeekStart)
+    ]).then(axios.spread((currentRes, lastWeekRes,populerInstaRes) => {
+        var gsDayNames = ['Pazar','Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi']
+        var currentResToplam = 0
+        for(var i=0; i < currentRes.data.stats.length; i++){
+            currentRes.data.stats[i].day = gsDayNames[(new Date(currentRes.data.stats[i].key_as_string)).getDay()]
+            currentResToplam = currentResToplam + currentRes.data.stats[i].doc_count
+        }
+        var lastWeekResTotal = 0
+        for(var i=0; i < lastWeekRes.data.stats.length; i++){
+            lastWeekResTotal = lastWeekResTotal + lastWeekRes.data.stats[i].doc_count
+        }
+        var oran = "";
+        if (currentResToplam > lastWeekResTotal) {
+            oran = "%"+((currentResToplam - lastWeekResTotal)/(currentResToplam)*100).toFixed(2)+" oranında artma";
+        } else {
+            oran = "%"+((lastWeekResTotal - currentResToplam)/(currentResToplam)*100).toFixed(2)+" oranında azalma";
+        }
+        var startDate = start_date.replace(/-/g, '.').split("T")[0]
+        var endDate = end_date.replace(/-/g, '.').split("T")[0]
+        res.render('instagram',{
+            start_date:startDate,
+            end_date:endDate,
+            currentRes:currentRes.data,
+            lastWeekRes:lastWeekRes.data,
+            currentResToplam:currentResToplam,
+            lastWeekResTotal:lastWeekResTotal,
+            oran:oran
+        });
+    }))
+})
 app.get('/pdf', (req, res) => {
     var pdfUrls = [
         {
@@ -183,6 +226,10 @@ app.get('/pdf', (req, res) => {
         {
             "name":"twitter",
             "url":"http://127.0.0.1:3000/twitter?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzkzNTEwNzUsIm5iZiI6MTU3OTM1MTA3NSwianRpIjoiMGEwNDcwYmItNTRjMy00MjczLWE4MzgtZGJmODdkNmJiOWE5IiwiaWRlbnRpdHkiOiJzZXJ2ZXRAYmlsZ2ltZWR5YS5jb20udHIiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.tIII43uoMHAm4f-2Ss7unjfFv7UMigRvAY0KxzO9wOo&stream_id=5debd0e928c70a000c7c3eb4&start_date=2020-01-12T20:21:00.000&end_date=2020-01-18T20:59:59.999"
+        },
+        {
+            "name":"instagram",
+            "url":"http://127.0.0.1:3000/instagram?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzkzNTEwNzUsIm5iZiI6MTU3OTM1MTA3NSwianRpIjoiMGEwNDcwYmItNTRjMy00MjczLWE4MzgtZGJmODdkNmJiOWE5IiwiaWRlbnRpdHkiOiJzZXJ2ZXRAYmlsZ2ltZWR5YS5jb20udHIiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.tIII43uoMHAm4f-2Ss7unjfFv7UMigRvAY0KxzO9wOo&stream_id=5debd0e928c70a000c7c3eb4&start_date=2020-01-12T20:21:00.000&end_date=2020-01-18T20:59:59.999"
         }
     ]
     console.log("PDF Hazırlanıyor");
